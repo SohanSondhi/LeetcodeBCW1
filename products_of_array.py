eda972d6b2ec5364354a3270cb2f46c854779d47
sohansondhi05@gmail.com <sohansondhi05@gmail.com>
2024-10-05 15:51:10 -0400

    leetcode q

---
+++ products_of_array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prod = [1]*len(nums)
+        left = 1
+        for i in range(len(nums)):
+            prod[i] = left
+            left *= nums[i]
+        
+        right = 1
+        for i in range((len(nums)-1),-1,-1):
+            prod[i] *= right
+            right *= nums[i]
+        return prod
